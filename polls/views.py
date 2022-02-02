c5120a0f8035377ae6d55994311e023fcbffa817
Defendi <alexandre_defendi@hotmail.com>
2022-02-02 20:13:38 -0300

    Criação das primeiras views dinamicas

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
+from .models import Question, Choice
+from django.template import loader
 
 def index(request):
-    return HttpResponse("<h1>Olá mundo. Você está no indice da aplicação de enquetes.</h1>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("Você está olhando para a pergunta %s." % question_id)
+
+def results(request, question_id):
+    response = "Você está vendo os resultados da pergunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na pergunta %s." % question_id)
