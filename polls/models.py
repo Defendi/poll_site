e743a7088b2694fafe1f5034338a442d96a82df5
Defendi <alexandre_defendi@hotmail.com>
2022-02-07 15:40:24 -0300

    Personalizando a aparência do site de administração

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,18 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
+
 
 class Question(models.Model):
+    
     question_text = models.CharField('Questão', max_length=200)
     pub_date = models.DateTimeField('Publicação')
     
     def __str__(self):
         return self.question_text
-    
+
+    @admin.display(boolean=True,ordering='pub_date',description='Recente Publicado?',)
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
